51589fd85d1840a803787e4ec6e90a5dabfac7a6
Binghua <vvebing@qq.com>
2021-01-11 23:28:28 +0800

    add ant design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uuid: localStorage.getItem('stocks-simulation::uuid'),
+      groupID: localStorage.getItem('stocks-simulation::groupID'),
+    };
+  }
 
-export default App;
+  render() {
+    const { uuid, groupID } = this.state;
+
+    return (
+      <div id="app">
+        
+      </div>
+    );
+  }
+}
